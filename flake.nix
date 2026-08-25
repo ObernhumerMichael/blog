f5e0776c1c8df4c39c52f04b3f59568ee451898b
Michael Obernhumer <michaelobernhumer@gmail.com>
2026-08-25 18:38:09 +0200

    feat: added nix dev environment

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Blog development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+
+        packages = [
+          pkgs.nodejs_26
+          pkgs.pnpm
+        ];
+
+        shellHook = ''
+          echo "🚀 Blog development environment"
+          node --version
+        '';
+      };
+    };
+}
